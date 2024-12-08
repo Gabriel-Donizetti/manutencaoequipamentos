61140f210970de0bb472b9b43f392f4401d94826
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2024-12-08 18:56:04 -0300

    persist admin

--- src/main/java/com/web2/manutencaoequipamentos/utils/DataLoader.java
+++ src/main/java/com/web2/manutencaoequipamentos/utils/DataLoader.java
@@ -29,28 +29,36 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-
         if (usuarioRepository.count() == 0) {
-
-            Usuario usuario = new Usuario();
-            usuario.setNome("Admin");
-            usuario.setEmail("admin@web2.com");
-            usuario.setSenha(passwordEncoder.encode("admin"));
-            Role roleAdmin = new Role();
-            roleAdmin.setName(Values.ROLE_ADMIN);
-            usuario.setRoles(Collections.singleton(roleAdmin));
-            usuarioRepository.saveAndFlush(usuario);
-
-            System.out.println("Usuário padrão criado com sucesso!");
-
-            Funcionario funcionario = new Funcionario();
-            funcionario.setDataNascimento(LocalDate.now());
-            funcionario.setUsuario(usuario);
-
-            funcionarioRepository.saveAndFlush(funcionario);
-            System.out.println("Funcionário padrão associado ao usuário criado com sucesso!");
+            criarUsuarioAdmin();
         } else {
             System.out.println("Usuários ou funcionários já existem no banco de dados.");
         }
+    }
+
+    private void criarUsuarioAdmin() {
+        // Cria a role ADMIN
+        Role roleAdmin = new Role();
+        roleAdmin.setName(Values.ROLE_ADMIN);
+
+        // Cria o usuário ADMIN
+        Usuario usuario = new Usuario();
+        usuario.setNome("Admin");
+        usuario.setEmail("admin@web2.com");
+        usuario.setSenha(passwordEncoder.encode("admin"));
+        usuario.setRoles(Collections.singleton(roleAdmin));
+
+        // Salva o usuário no banco
+        usuarioRepository.save(usuario);
+        System.out.println("Usuário padrão criado com sucesso!");
+
+        // Associa o usuário a um funcionário
+        Funcionario funcionario = new Funcionario();
+        funcionario.setDataNascimento(LocalDate.now());
+        funcionario.setUsuario(usuario);
+
+        // Salva o funcionário no banco
+        funcionarioRepository.save(funcionario);
+        System.out.println("Funcionário padrão associado ao usuário criado com sucesso!");
     }
 }
