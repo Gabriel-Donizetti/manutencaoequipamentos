91bd10350ea104bc5603ca4e4b98c55c23f8fc78
Gabriel-Donizetti <gabsdonizetti@gmail.com>
2024-12-09 00:38:44 -0300

    fix

--- src/main/java/com/web2/manutencaoequipamentos/solicitacao/SolicitacaoRepository.java
+++ src/main/java/com/web2/manutencaoequipamentos/solicitacao/SolicitacaoRepository.java
@@ -10,7 +10,7 @@ import com.web2.manutencaoequipamentos.funcionario.Funcionario;
 
 public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long> {
 
-    @Query(value = "SELECT funcionario_id, COUNT(*) AS quantidade_solicitacoes " +
+    @Query(value = "SELECT funcionario_idfuncionario, COUNT(*) AS quantidade_solicitacoes " +
             "FROM solicitacao GROUP BY funcionario_id ORDER BY quantidade_solicitacoes ASC LIMIT 1", nativeQuery = true)
     Funcionario findFuncionarioComMenosSolicitacoes();
 
